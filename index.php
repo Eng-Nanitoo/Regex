7e6bb961440487696bb44d9f705258576a2bd5fc
Eng-Nanitoo <i20378.etu@iscae.mr>
2025-05-10 20:26:43 +0000

    Adding php files

--- index.php
+++ index.php
@@ -8,17 +8,17 @@
 </head>
 </body>
 <main>
-    <form action="/user/auth/login" method="POST" id="form">
+    <form action="/Compilation/php/upload.php" method="POST" id="form">
         <div>
-                <input type="text" placeholder="Marque" oninput="checkMarque()" id="marque">
+                <input type="text" placeholder="Marque" name="marque" oninput="checkMarque()" id="marque">
                 <p id="marqueError"></p>
             </div>
             <div>
-                <input type="text" placeholder="Matricule" oninput="checkMatricule()" id="matricule">
+                <input type="text" placeholder="Matricule" name="matricule"  oninput="checkMatricule()" id="matricule">
                 <p id="matriculeError"></p>
             </div>
             <div class="submit">
-                <button type="submit">Enregistrer</button>
+                <button type="submit" name="submit">Enregistrer</button>
             </div>
         </form>
     </main>
---
+++ php/cnx.php
@@ -0,0 +1,8 @@
+<?php
+    $db = "regexdb";
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+
+    $conn = mysqli_connect($host, $user, $password, $db);
+?>
